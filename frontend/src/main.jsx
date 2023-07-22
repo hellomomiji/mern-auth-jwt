c07d5fa6b347e553131ca713ed36a08d4ecd44e2
Yang Jiang <jiangyang.jnu@gmail.com>
2023-07-22 15:15:51 -0700

    setup frontend, redux for login/logout/register

---
+++ frontend/src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import store from './store.js';
+import { Provider } from 'react-redux';
+
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import HomeScreen from './Screens/HomeScreen.jsx';
+import LoginScreen from './Screens/LoginScreen.jsx';
+import RegisterScreen from './Screens/RegisterScreen.jsx';
+import ProfileScreen from './Screens/ProfileScreen.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      <Route path='/profile' element={<ProfileScreen />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+  </Provider>
+);
